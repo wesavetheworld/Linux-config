8f18ebc7239dd1f01ec0d1e67e73ef0759c4321d
Johan Geluk <jhgeluk@gmail.com>
2015-06-01 10:37:08 +0200

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-def isYes(input):
+def is_yes(input):
     lower = input.lower()
     return lower == "y" || lower == "yes"
 
@@ -10,8 +10,12 @@ print("Before you begin, please ensure that you've installed the following appli
 print(" ".join(apps) + "\n")
 print("Do you wish to continue? (y/n)")
 
+def move_recursive(source, destination):
+    
+
 if(isYes(raw_input())):
     # install code goes here
+    move_recursive("home", "~")
 else:
     print("You'll probably want to use one of these to install them, then:")
     print("sudo pacman -S " + " ".join(apps))
