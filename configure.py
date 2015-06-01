bf0e1140b9d2a218cf8abfc0afbeaee3f39e8650
Johan Geluk <jhgeluk@gmail.com>
2015-06-01 10:44:54 +0200

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
+import shutil
+
+
+# function definitions
 
 def is_yes(input):
     lower = input.lower()
     return lower == "y" || lower == "yes"
+    
+def move_recursive(source, destination):
+    shutil.copytree(source, destination)
+    shutil.rmtree(source)
+
+
+# field definitions
 
 apps = ["vim", "zsh"]
+
+
+# configuration script
 
 print("Before you begin, please ensure that you've installed the following applications:")
 print(" ".join(apps) + "\n")
 print("Do you wish to continue? (y/n)")
 
-def move_recursive(source, destination):
-    
-
 if(isYes(raw_input())):
     # install code goes here
     move_recursive("home", "~")
+    
+    # clean up the repo
+    shutil.rmtree(".git")
+    
+    
 else:
     print("You'll probably want to use one of these to install them, then:")
     print("sudo pacman -S " + " ".join(apps))
