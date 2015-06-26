6566a7677f279b02ced35b4ad5a17d2fc336e135
Johan Geluk <jhgeluk@gmail.com>
2015-06-26 19:46:48 +0200

    Update configure.py

--- configure.py
+++ configure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import shutil
 import os
+import sys
 
 # function definitions
 
@@ -19,33 +20,37 @@ apps = ["git", "vim", "zsh"]
 print("Before you begin, please ensure that you've installed the following applications:")
 print(" ".join(apps) + "\n")
 print("Do you wish to continue? (y/n)")
+if !is_yes(input()):
+    sys.exit(0)
 
-if is_yes(input()):
-    for file in os.listdir("home"):
-        location = os.path.join("home", file)
-        home = os.path.expanduser("~")
-        if os.path.isdir(location):
-            print("moving dir '" + location + "' to '" + home + "'")
-            shutil.copytree(location, home)
-            shutil.rmtree(location)
-        else:
-            print("moving file '" + location + "' to '" + home + "'")
-            shutil.move(location, home)
-    
-    for file in os.listdir("root"):
-        location = os.path.join("root", file)
-        root = "/"
-        if os.path.isdir(location):
-            print("moving dir '" + location + "' to '" + home + "'")
-            shutil.copytree(location, home)
-            shutil.rmtree(location)
-        else:
-            print("moving file '" + location + "' to '" + home + "'")
-            shutil.move(location, home)
-    print("Cleaning up...");
-    shutil.rmtree("../linux-config")
-    os.chdir("..")
-    print("All done!")
+
+for file in os.listdir("home"):
+    location = os.path.join("home", file)
+    home = os.path.expanduser("~")
+    if os.path.isdir(location):
+        print("moving dir '" + location + "' to '" + home + "'")
+        shutil.copytree(location, home)
+        shutil.rmtree(location)
+    else:
+        print("moving file '" + location + "' to '" + home + "'")
+        shutil.move(location, home)
+
+print("User setup complete. Would you like to configure global settings as well? (y/n)")
+    if is_yes(input()):
+        for file in os.listdir("root"):
+            location = os.path.join("root", file)
+            root = "/"
+            if os.path.isdir(location):
+                print("moving dir '" + location + "' to '" + home + "'")
+                shutil.copytree(location, home)
+                shutil.rmtree(location)
+            else:
+                print("moving file '" + location + "' to '" + home + "'")
+                shutil.move(location, home)
+print("Cleaning up...");
+os.chdir("..")
+shutil.rmtree("linux-config")
+print("All done!")
     
 else:
     print("You'll probably want to use one of these to install them, then:")
